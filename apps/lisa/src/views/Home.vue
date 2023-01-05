e09f1a2e64e80ee1d5f3b109ea5cf2f6656f9427
tobitubbie <triplttt@gmail.com>
2023-01-05 15:50:05 +0100

    use setup-script + remove unnecessary code (isHttp, content)

--- apps/lisa/src/views/Home.vue
+++ apps/lisa/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
+
   <div class="grid">
     <div class="col lg:col-6 lg:col-offset-3">
+
       <div class="p-inputgroup">
         <InputText placeholder="A URI to do actions on." v-model="uri" @keyup.enter="fetch"/>
         <Button @click="fetch"> GET</Button>
       </div>
+
       <div class="progressbarWrapper">
         <ProgressBar v-if="isLoading" mode="indeterminate"/>
       </div>
+
     </div>
   </div>
+
   <div class="demands">
     <span>
       <h3> Demands </h3>
@@ -19,88 +24,51 @@
       <div v-if="demandsAvailable">No demands available.</div>
     </span>
   </div>
+
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import {useToast} from "primevue/usetoast";
 import {useSolidSession} from "@shared/composables";
 import {getResource, LDP, parseToN3} from "@shared/solid";
-import {computed, defineComponent, ref, toRefs} from "vue";
+import {ref} from "vue";
 import DemandProcessor from "../components/DemandProcessor.vue";
 
 
-export default defineComponent({
-  name: "Home",
-  components: {DemandProcessor},
-  setup(props, context) {
-    const toast = useToast();
-    const {authFetch, sessionInfo} = useSolidSession();
-    const {isLoggedIn, webId} = toRefs(sessionInfo);
-    const isLoading = ref(false);
-    const demandsAvailable = ref(false);
+const toast = useToast();
+const {authFetch, sessionInfo} = useSolidSession();
+const isLoading = ref(false);
+const demandsAvailable = ref(false);
 
-    // uri of the information resource
-    const uri = ref("");
-    uri.value = "https://bank.solid.aifb.kit.edu/credits/demands/";
-    // watch(
-    //   () => inbox.value,
-    //   () => (uri.value = inbox.value),
-    //   { immediate: true }
-    // );
-    const isHTTP = computed(
-        () => uri.value.startsWith("http://") || uri.value.startsWith("https://")
-    );
-    // content of the information resource
-    const content = ref("");
-    //   content.value =
-    //     "This is a demo resource, which you only have access to after you 'unlock' it with a Verifiable Credential issued by the creator of this demo: Alice aka. Christoph aka. uvdsl :)\n\nClick `GET` to access the resource.\n\n    If you get a 401, log in\n                               (button at the top right).\n\n    If you get a 403, unlock the resource\n                               (button at the bottom).";
-    // watch(
-    //   () => inbox.value,
-    //   () => (content.value = inbox.value !== "" ? "<#this> a <#demo>." : ""),
-    //   { immediate: true }
-    // );
-    // get content of information resource
-    let demands = ref()
-    const fetch = async () => {
-      if (!isHTTP.value) {
-        return;
-      }
-      isLoading.value = true;
-      await getResource(uri.value, authFetch.value)
-          .catch((err) => {
-            toast.add({
-              severity: "error",
-              summary: "Error on fetch!",
-              detail: err,
-              life: 5000,
-            });
-            isLoading.value = false;
-            throw new Error(err);
-          })
-          .then((resp) => resp.text()).then((txt) => {
-            return parseToN3(txt, uri.value)
-          }).then((parsedN3) => {
-            return parsedN3.store;
-          }).then((store) => {
-            demands.value = store.getObjects(uri.value, LDP("contains"), null).map((node) => node.value);
-          })
-          .finally(() => {
-            isLoading.value = false;
-          })
-    };
+// uri of the information resource
+const uri = ref("https://bank.solid.aifb.kit.edu/credits/demands/");
 
-
-    return {
-      uri,
-      fetch,
-      content,
-      isLoading,
-      isLoggedIn,
-      demands,
-      demandsAvailable
-    };
-  },
-});
+// get content of information resource
+let demands = ref()
+const fetch = async () => {
+  isLoading.value = true;
+  await getResource(uri.value, authFetch.value)
+      .catch((err) => {
+        toast.add({
+          severity: "error",
+          summary: "Error on fetch!",
+          detail: err,
+          life: 5000,
+        });
+        isLoading.value = false;
+        throw new Error(err);
+      })
+      .then((resp) => resp.text()).then((txt) => {
+        return parseToN3(txt, uri.value)
+      }).then((parsedN3) => {
+        return parsedN3.store;
+      }).then((store) => {
+        demands.value = store.getObjects(uri.value, LDP("contains"), null).map((node) => node.value);
+      })
+      .finally(() => {
+        isLoading.value = false;
+      })
+};
 
 // const demandsAvailable = computed(() => // TODO
 
