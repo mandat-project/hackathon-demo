25276059bf93716d5fe57dc19bcbe531299ed77a
tobitubbie <triplttt@gmail.com>
2023-04-17 12:13:25 +0200

    [lisa] fixes fetch of demandUris + fixes shape-tree-uri + removes uri-input from template

--- apps/lisa/src/views/Home.vue
+++ apps/lisa/src/views/Home.vue
@@ -1,23 +1,6 @@
 <template>
 
   <div class="grid">
-    <div class="col lg:col-6 lg:col-offset-3">
-
-      <div class="p-inputgroup">
-        <InputText
-            placeholder="URI of the banks' credit-demands."
-            v-model="uri"
-            @keyup.enter="fetchDemandUris()"/>
-
-        <Button @click="fetchDemandUris()"> GET</Button>
-      </div>
-
-      <ProgressBar v-if="isLoading" mode="indeterminate" class="progressbar"/>
-
-    </div>
-  </div>
-
-  <div class="grid">
     <h1 class="col-12 flex align-items-center gap-2">
       Demands
       <Button icon="pi pi-refresh" class="p-button-text p-button-rounded p-button-icon-only" @click="fetchDemandUris()"/>
@@ -42,7 +25,7 @@ import DemandProcessor from "../components/DemandProcessor.vue";
 const toast = useToast();
 const {authFetch, sessionInfo} = useSolidSession();
 
-const uri = ref("https://bank.solid.aifb.kit.edu/credits/demands/");
+const shapeTreeUri = 'https://solid.aifb.kit.edu/shapes/mandat/credit.tree#creditDemandTree';
 const webId = 'https://bank.solid.aifb.kit.edu/profile/card#me';
 const isLoading = ref(false);
 const demandUris = ref<string[]>([]);
@@ -50,36 +33,31 @@ const demandUris = ref<string[]>([]);
 // refetch demandUris on login
 watch(() => sessionInfo.isLoggedIn, (isLoggedIn) => isLoggedIn ? fetchDemandUris() : {});
 
-async function fetchDemandUris(): Promise<string[]> {
 
-    const shapeTreeUri = 'https://solid.aifb.kit.edu/shapes/mandat/credit.tree#creditOfferTree';
-    const containers = await getDataRegistrationContainers(webId, shapeTreeUri, authFetch.value);
+// discovers all containers including demands and add their contents (demands) to demandUris
+async function fetchDemandUris(): Promise<void> {
 
   demandUris.value = [];
   isLoading.value = true;
-  return containers.forEach(containerUri => getResource(containerUri, authFetch.value)
-      .catch((err) => {
-          toast.add({
-              severity: "error",
-              summary: "Error on fetch!",
-              detail: err,
-              life: 5000,
-          });
-          isLoading.value = false;
-          throw new Error(err);
-      })
-      .then(resp => resp.text())
-      .then(txt => parseToN3(txt, uri.value))
-      .then(parsedN3 => parsedN3.store)
-      .then(store => demandUris.value = store.getObjects(uri.value, LDP("contains"), null).map((node) => node.value))
-      .finally(() => isLoading.value = false)) ;
+
+  getDataRegistrationContainers(webId, shapeTreeUri, authFetch.value)
+      .then(containerUris => containerUris.forEach(containerUri => getResource(containerUri, authFetch.value)
+          .catch((err) => {
+              toast.add({
+                  severity: "error",
+                  summary: "Error on fetch!",
+                  detail: err,
+                  life: 5000,
+              });
+              isLoading.value = false;
+              throw new Error(err);
+          })
+          .then(resp => resp.text())
+          .then(txt => parseToN3(txt, containerUri))
+          .then(parsedN3 => parsedN3.store)
+
+          .then(store => demandUris.value.push(...store.getObjects(containerUri, LDP("contains"), null).map((node) => node.value)))
+      ))
+      .finally(() => isLoading.value = false);
 }
 </script>
-
-<style scoped>
-.progressbar {
-  height: 2px;
-  border-radius: 0 0 3px 3px;
-  transform: translateY(-2px);
-}
-</style>
