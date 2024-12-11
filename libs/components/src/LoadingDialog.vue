78a9a228e82f273bece44bf331f87bc6649b5a8d
banxtv <slashdancelol@googlemail.com>
2024-12-11 06:55:26 +0100

    change LoadingDialog.vue text

--- libs/components/src/LoadingDialog.vue
+++ libs/components/src/LoadingDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <Dialog :visible="props.isVisible" center header="Redirecting to the AuthApp" modal
           class="w-auto mt-4 mx-auto block">
-    <p class="mb-5 mt-0">Please wait. You will be redirecting to the AuthApp...</p>
+    <p class="mb-5 mt-0">Please wait. You will be redirected to the AuthApp...</p>
   </Dialog>
 </template>
 <style scoped>
